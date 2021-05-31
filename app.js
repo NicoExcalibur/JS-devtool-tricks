e5c4ce7affcdb5e09d8f32109ee8fa948c2e809f
NicoExcalibur <nico.zanz@gmail.com>
2021-05-31 17:43:10 +0200

    console.log tricks

--- app.js
+++ app.js
@@ -10,25 +10,55 @@ function makeGreen() {
 }
 
 // Regular
+console.log("hello");
 
 // Interpolated
+console.log("Hello I am a %s string", "💩");
 
 // Styled
+console.log("%c I am some great text", "font-size: 20px", "background: red");
 
 // warning!
+console.warn("oh noooo");
 
 // Error :|
+console.error("shit!");
 
 // Info
+console.info("Crocodiles eat 3-4 people per year");
 
 // Testing
+const p = document.querySelector("p");
+console.assert(p.classList.contains("ouch"), "That is wrong");
 
 // clearing
+console.clear();
 
 // Viewing DOM Elements
+console.log(p);
+console.dir(p);
+console.clear();
 
 // Grouping together
+dogs.forEach((dog) => {
+  console.group(`${dog.name}`);
+  console.log(`This is ${dog.name}`);
+  console.log(`${dog.name} is ${dog.age} years old`);
+  console.log(`${dog.name} is ${dog.age * 7} dogs years old`);
+  console.groupEnd();
+});
 
 // counting
+console.count("Wes");
+console.count("Wes");
+console.count("steve");
+console.count("Wes");
+console.count("Wes");
 
 // timing
+console.time("Fetching data");
+fetch("https://api.github.com/users/wesbos")
+  .then((data) => data.json())
+  .then((data) => {
+    console.timeEnd("Fetching data");
+  });
